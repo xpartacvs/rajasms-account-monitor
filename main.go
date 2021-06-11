6072d0084753bd9354f963cb30feb646f8d6695f
Yudi Primaputra <xpartacvs@gmail.com>
2021-06-11 20:31:26 +0700

    feat(package): Add main package

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"rajasms-account-monitor/packages/logger"
+	"rajasms-account-monitor/packages/worker"
+)
+
+func main() {
+	logger.Log().Info().Msg("Application is starting...")
+
+	if err := worker.Start(); err != nil {
+		logger.Log().Err(err).Msg("Application has been terminated due to unexpected error.")
+	}
+
+	logger.Log().Info().Msg("Good bye!")
+}
